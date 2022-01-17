56c6d2426e0486ca2611781195a119c56febedcc
Alex Serbin <daldonchik@gmail.com>
2022-01-17 22:32:10 +0200

    add papku s dz 2

---
+++ Dz_nomer2/dz_nomer2.py
@@ -0,0 +1,6 @@
+print("""Escape sequnces
+\\a\t\t\tBell (alert)\n\\b\t\t\tBackspace
+\\n\t\t\tNew line\n\\t\t\t\tHorizontal tab
+\\\\\t\t\tBackslash \\
+\\\"\t\t\tDouble quotation mark \"\n\\\'\t\t\tSingle quotation mark\'""")
+
