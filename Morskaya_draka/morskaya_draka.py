1b009725c905ce06900e8ebd26030f87dd2b10e4
Alex Serbin <daldonchik@gmail.com>
2022-02-23 19:18:49 +0200

    some changes

--- Morskaya_draka/morskaya_draka.py
+++ Morskaya_draka/morskaya_draka.py
@@ -1,23 +1,60 @@
-ver = hor = 11
-# ━ ┃ ┣ ┫  ┛ ┗ ┓ ┏ ┳ ┻  ╋ │ ┤
+ver = hor = 21
+
+# ━ ┃ ┣ ┫  ┛ ┗ ┓ ┏ ┳ ┻  ╋ │ ┤ ▅   ▀▀▄▅▆▇█▉▊▋  ▉
+b = []
 for i in range(ver):
     for k in range(hor):
         if i == 0 and k == hor - 1:
             print('━┓', end='')
+            b.append('━┓')
         elif k == 0 and i == 0:
             print('┏━', end='')
+            b.append('┏━')
         elif k == 0 and i == ver - 1:
             print('┗━', end='')
-        elif k == 0:
+            b.append('┗━')
+        elif k == 0 and i % 2 == 0:
             print('┣━', end='')
+            b.append('┣━')
+        elif k == 0:
+            print('┃ ', end='')
+            b.append('┃ ')
         elif k == hor - 1 and i == ver - 1:
             print('━┛', end='')
-        elif k == hor - 1:
+            b.append('━┛')
+        elif k == hor - 1 and i % 2 == 0:
             print('━┫',end='')
-        elif i == ver - 1:
+            b.append('━┫')
+        elif k == hor - 1:
+            print(' ┃',end='')
+            b.append(' ┃')
+        elif i == ver - 1 and k % 2 == 0:
             print('━┻━',end='')
-        elif i == 0:
+            b.append('━┻━')
+        elif i == 0 and k % 2 == 0:
             print('━┳━',end='')
+            b.append('━┳━')
+        elif i == 0 or i % 2 == 0 and k % 2 == 1 or i == ver - 1:
+            print('━━━',end='')
+            b.append('━━━')
+        elif i % 2 == 1 and k % 2 == 1:
+            print('   ',end='')
+            b.append('   ')
+        elif i % 2 == 1 and k % 2 == 0:
+            print(' ┃ ',end='')
+            b.append(' ┃ ')
         else:
             print('━╋━', end='')
+            b.append('━╋━')
     print()
+    b.append('\n')
+# k = 0
+# for i in range(len(b)):
+#     if b[i] == '   ':
+#         print(i)
+#         k +=1
+# print(k)
+
+
+b[23] = "███"
+print (''.join(b))
